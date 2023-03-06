7fc404400e6500b104add41e08be492c4f552afc
aaronmueller <ammaaronmueller@gmail.com>
2023-03-06 15:30:48 -0500

    update notation in bash scripts (for colab)

--- finetune_all_tasks.sh
+++ finetune_all_tasks.sh
@@ -11,5 +11,5 @@ SEED=${7:-12}
 # use default hyperparameters
 for task in {"cola","sst2","mrpc","qqp","mnli","mnli-mm","qnli","rte","boolq","multirc","wsc"}; do
     # qsub -q g.q -cwd -j y -l hostname="b1[123456789]|c0*|c1[13456789],ram_free=10G,mem_free=10G,gpu=1" finetune_model.sh $MODEL_PATH $task
-	finetune_model.sh $MODEL_PATH $task $LR $PATIENCE $BSZ $EVAL_EVERY $MAX_EPOCHS $SEED
+	./finetune_model.sh $MODEL_PATH $task $LR $PATIENCE $BSZ $EVAL_EVERY $MAX_EPOCHS $SEED
 done
